fa05514d44fe6884d3d5b563f4fb68621298b3ef
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2015-04-21 21:22:23 -0500

    se agrega una prueba

--- PruebaGit2/src/main/java/edu/co/sena/pruebagit2/pruebagit2/tioweowetr.java
+++ PruebaGit2/src/main/java/edu/co/sena/pruebagit2/pruebagit2/tioweowetr.java
@@ -1,4 +1,3 @@
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,6 +9,6 @@ package edu.co.sena.pruebagit2.pruebagit2;
  *
  * @author hernando
  */
-public class Laborrada {
-    // esto no va a salir en el codigo que le hago commit
+public class tioweowetr {
+    
 }
