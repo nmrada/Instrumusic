9e08941a7b4f7247a9228024179f9837f8c3d254
ColsutecR <ColsutecR@ColsutecR-PC>
2015-04-22 17:25:32 -0500

    Escribi Mi Linea

--- PruebaGit2/src/main/java/edu/co/sena/pruebagit2/pruebagit2/Daniel.java
+++ PruebaGit2/src/main/java/edu/co/sena/pruebagit2/pruebagit2/Daniel.java
@@ -10,8 +10,8 @@ package edu.co.sena.pruebagit2.pruebagit2;
  * @author ColsutecR
  */
 public class Daniel {
+    //Esta Line La Hizo Juancho
     //Esta linea esta inscrita por daniel
-    
     // Esta linea la hizo Nicolas
     //esta linea la hizo el profesor hernando
     //esta linea la hizo Johana y Maicol
