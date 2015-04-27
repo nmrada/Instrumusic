771097f3c8ed8d7e48fb6b705246ce7adab0d91d
ColsutecR <ColsutecR@ColsutecR-PC>
2015-04-27 17:42:33 -0500

    Se agrego la interfaz Log4JConfiguration.

---
+++ Codigo/TiendaEnLineaJPAEclipseLink/src/main/java/edu/co/sena/instrumusic/general/util/Log4JConfiguration.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.co.sena.instrumusic.general.util;
+
+/**
+ *
+ * @author ColsutecR
+ */
+public interface Log4JConfiguration {
+
+    public static final String PROPERTIES = "log4j.properties";
+}
